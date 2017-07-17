9edd4a02757f46527db762479d2ca28eb6d0548e
Flowmar <omar.imam@kineticvortex.com>
2017-07-16 21:31:32 -0500

    Add transitions, buttons, contact form

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,4 @@
 
-var first = $('#first')
-
-function pageLoad() {
-    first.setTimeout(function () {
-        hide();
-    }, 7000, function () {
-        first.setTimeout(function () {
-            first.show();
-        }, 5000)
-    })
-};
-
 $(document).ready(function () {
     $("body").addClass("loaded");
     pageLoad();
