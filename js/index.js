d54905fc72bc9df2cdeef990efe238944b43b32a
Flowmar <omar.imam@kineticvortex.com>
2017-09-14 23:27:33 -0500

    add animations, fix navbar, add style changes, add final links for wireframing project

--- js/index.js
+++ js/index.js
@@ -1,14 +1,56 @@
-$(window).resize(function () {
-    var viewportWidth = $(window).width();
+var viewportWidth = $(window).width();
+
+$(document).ready(function() {
+    if (viewportWidth < 576) {
+        $('.navbtn').show();
+    }
+    if (viewportWidth >= 576) {
+        $('.nav-item').addClass('list-inline-item');
+        $('.navbtn').show();
+        $('.nav-list').addClass('list-inline btn-lg');
+    }
     if (viewportWidth >= 768) {
-        $(".navbar-right").removeClass("navbar-nav");
-        $('.navbar').addClass("navbar-tablet");
+        $('.nav-list').addClass('list-inline');
+        $('.nav-link').addClass('btn-lg');
+    }
+    if (viewportWidth >= 992) {
+        // $('.navbtn').addClass('hidden');
+    }
+    if (viewportWidth >= 1200) {
+        // $('.navbtn').addClass('hidden');
     }
 });
 
-$(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth >= 1100) {
-        $("#flow").addClass("moveFlow mt-5");
-    }
+$(document).ready(function() {
+    $(window).resize(function() {
+        if (viewportWidth < 576) {
+            $('.navbtn').show();
+            $('.nav-list')
+                .removeClass('list-inline')
+                .addClass('navbar-nav');
+        }
+        if (viewportWidth >= 576) {
+            $('.nav-list')
+                .removeClass('navbar-nav')
+                .addClass('list-inline');
+            $('.nav-link').addClass('btn-lg');
+            $('.nav-item').addClass('list-inline-item');
+            $('.navbtn').addClass('hidden');
+        }
+        if (viewportWidth >= 768) {
+            $('.nav-list').addClass('list-inline justify-content-center navbar-tablet navbar-nav');
+        }
+    });
 });
+
+$(document).ready(function() {
+    $('#code-link').on('click', function() {
+        $('button').addClass('rollOut');
+    });
+});
+// $(window).resize(function() {
+// 	var viewportWidth = $(window).width();
+// 	if (viewportWidth >= 1100) {
+// 		$('#flow').addClass('moveFlow mt-5');
+// 	}
+// });
